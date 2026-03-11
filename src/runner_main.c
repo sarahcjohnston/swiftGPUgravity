4d279007be0dbbf02152006d9c2193b52fd9c978
wjr21 <wjr21@sussex.ac.uk>
2026-03-11 15:30:12 +0000

    Temp add type string

--- src/runner_main.c
+++ src/runner_main.c
@@ -1762,7 +1762,8 @@ void* runner_main(void* data) {
           runner_do_rt_advance_cell_time(r, t->ci, 1);
           break;
         default:
-          error("Unknown/invalid task type (%d).", t->type);
+          error("Unknown/invalid task type (%d/%d:%s/%s).",
+                taskID_names[t->type], subtaskID_names[t->subtype]);
       }
 
       /*ntasks = sched->queues[r->qid].count; //how many tasks there are to do
